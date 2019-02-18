e3521d819a4641cb9455ab83b5b9b4bc237f83d7
Keith Gill <kgill@kimray.com>
2019-02-18 15:45:08 -0600

    - kgill:: Create the app for Kimray Symposium 2019...

--- KimraySymposium/Form1.Designer.cs
+++ KimraySymposium/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PrintCSV = new System.Windows.Forms.Button();
+            this.bnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PrintCSV
+            // 
+            this.PrintCSV.Location = new System.Drawing.Point(73, 43);
+            this.PrintCSV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PrintCSV.Name = "PrintCSV";
+            this.PrintCSV.Size = new System.Drawing.Size(220, 39);
+            this.PrintCSV.TabIndex = 0;
+            this.PrintCSV.Text = "Print the List !";
+            this.PrintCSV.UseVisualStyleBackColor = true;
+            this.PrintCSV.Click += new System.EventHandler(this.PrintCSV_Click);
+            // 
+            // bnCancel
+            // 
+            this.bnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bnCancel.Location = new System.Drawing.Point(146, 174);
+            this.bnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bnCancel.Name = "bnCancel";
+            this.bnCancel.Size = new System.Drawing.Size(112, 32);
+            this.bnCancel.TabIndex = 1;
+            this.bnCancel.Text = "Cancel";
+            this.bnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.PrintCSV;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.CancelButton = this.bnCancel;
+            this.ClientSize = new System.Drawing.Size(358, 117);
+            this.Controls.Add(this.bnCancel);
+            this.Controls.Add(this.PrintCSV);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "2019 Kimray Symposium List";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button PrintCSV;
+        private System.Windows.Forms.Button bnCancel;
     }
 }
 
